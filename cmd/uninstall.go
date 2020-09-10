57ffb2c1f68fce998d38283b3f479d35dad5b479
Daniel Helfand <helfand.4@gmail.com>
2020-09-10 14:58:10 -0500

    fix error message for when components are not installed

--- cmd/uninstall.go
+++ cmd/uninstall.go
@@ -61,45 +61,34 @@ func uninstall(args []string) error {
 		}
 	}
 
+	if len(componentVersions) == 0 {
+		fmt.Println("No components on cluster to uninstall")
+		return nil
+	}
+
 	return uninstallComponents(componentVersions)
 }
 
 func getComponentVersion(component string, all bool) (string, error) {
+	var argv []string
 	if component != dashboard {
 		// Since deployment for pipeline is named tekton-pipelines-controller, reassign component name to pipelines
 		if component == pipeline {
 			component = "pipelines"
 		}
-
-		argv := []string{"get", "deployment/tekton-" + component + "-controller", "-o", "jsonpath={.metadata.labels['app\\.kubernetes\\.io/version']}", "-n", "tekton-pipelines"}
-		kubectlCmd := exec.Command("kubectl", argv...)
-		kubectlCmd.Env = os.Environ()
-		kubectlCmd.Stderr = os.Stderr
-
-		version, err := kubectlCmd.Output()
-		if err != nil {
-			if all {
-				return "", nil
-			}
-
-			return "", fmt.Errorf("failed to get version of component %s", component)
-		}
-
-		return string(version), nil
+		argv = []string{"get", "deployment/tekton-" + component + "-controller", "-o", "jsonpath={.metadata.labels['app\\.kubernetes\\.io/version']}", "-n", "tekton-pipelines"}
+	} else {
+		argv = []string{"get", "deployment/tekton-" + component, "-o", "jsonpath={.metadata.labels.version}", "-n", "tekton-pipelines"}
 	}
 
-	argv := []string{"get", "deployment/tekton-" + component, "-o", "jsonpath={.metadata.labels.version}", "-n", "tekton-pipelines"}
 	kubectlCmd := exec.Command("kubectl", argv...)
-	kubectlCmd.Env = os.Environ()
-	kubectlCmd.Stderr = os.Stderr
-
 	version, err := kubectlCmd.Output()
 	if err != nil {
 		if all {
 			return "", nil
 		}
 
-		return "", fmt.Errorf("failed to get version of component %s", component)
+		return "", fmt.Errorf("failed to get version of component %s, check if it is installed", component)
 	}
 
 	return string(version), nil
